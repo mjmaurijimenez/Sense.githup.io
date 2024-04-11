b26ddf666ed6c23003774dadd38b1bfdcd56070a
mjmaurijimenez <147302798+mjmaurijimenez@users.noreply.github.com>
2024-04-11 13:13:26 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('sticky', this.window.scrollY > 80);
+});
+
+// Open menu
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+// Solucion
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+// scrollReveal 
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '85px',
+    duration: 2500,
+    reset: false
+}); 
+
+sr.reveal('.home-text',{delay:300});
+sr.reveal('.home-img',{delay:400});
+sr.reveal('.container',{delay:400});
+
+sr.reveal('.about-img',{});
+sr.reveal('.about-text',{delay:300});
+
+sr.reveal('.middle-text',{});
+sr.reveal('.row-btn,.shop-content',{delay:300});
+
+sr.reveal('.review,.contact',{delay:300});
+
